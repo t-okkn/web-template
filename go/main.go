4a49e03cbb141683c54c98f9b9270c3b96d3c970
t-okkn <okkn@okkn.net>
2021-12-10 12:39:31 +0900

    [feat] version情報を表示できるように更新

--- go/main.go
+++ go/main.go
@@ -1,6 +1,10 @@
 package main
 
-// import "nginx/unit"
+import (
+	"flag"
+	"fmt"
+	// "nginx/unit"
+)
 
 const LISTEN_PORT string = ":8501"
 
@@ -12,7 +16,13 @@ var (
 // summary => main関数（サーバを開始します）
 /////////////////////////////////////////
 func main() {
+	flag.Parse()
+
+	if flag.Arg(0) == "version" {
+		fmt.Println(Version, Revision)
+		return
+	}
+
 	// unit.ListenAndServe(LISTEN_PORT, SetupRouter())
 	SetupRouter().Run(LISTEN_PORT)
 }
-
